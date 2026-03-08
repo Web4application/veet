7f64728616cc401746aa1655ba3ab6dc237c00c0
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-03-08 21:46:06 +0100

    Create brew.sh

---
+++ brew.sh
@@ -0,0 +1,13 @@
+npm create vite@latest
+# npm 7+, extra double-dash is needed:
+npm create vite@latest my-vue-app -- --template vue vanilla, vanilla-ts, vue, vue-ts, react, react-ts, react-swc, react-swc-ts, preact, preact-ts, lit, lit-ts, svelte, svelte-ts, solid, solid-ts, qwik, qwik-ts
+
+
+
+
+
+
+
+
+
+
